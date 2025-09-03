5e05112cd4e2de1b4b018125cf8fdf349249df19
SadhviNarayanan <146152067+SadhviNarayanan@users.noreply.github.com>
2025-09-02 17:40:45 -0700

    Lab1 mcu (#3)
    * added MCU source code
    
    * Added Lab 1 FPGA code
    
    * adding all the relevant verilog files and testbench code
    
    * adding the final top level testbench and testvectors
    
    * removed unused clock signal

--- fpga/src/lab1_sn.sv
+++ fpga/src/lab1_sn.sv
@@ -1,23 +1,49 @@
-module top(
-	input 	logic	mcu_blink_in,
-	output 	logic fpga_blink_out, mcu_echo_led
+// E155, Lab 1 Code to control led's and connect to a seven segment display
+
+// Name: Sadhvi Narayanan
+// Email: sanarayanan@g.hmc.edu
+// Date: 08/27/2025
+
+
+// top level module to initialize the led's including the 2.4Hz one, and instantiate the seven segment display module
+module lab1_sn(
+	 input logic reset,
+	 input logic [3:0] s,
+	 output logic [2:0] led,
+	 output logic [6:0] seg
 );
 
-	logic int_osc;
-	logic pulse;
-	logic led_state = 0;
-	logic [24:0] counter = 0;
-	
-	// Internal high-speed oscillator
-	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
-	
-	// Simple clock divider
-	always_ff @(posedge int_osc)
-		begin
-			counter <= counter + 1;
-		end
-		
-  assign fpga_blink_out = counter[24];
-	assign mcu_echo_led = mcu_blink_in;
-
+   logic int_osc;
+   logic [23:0] counter;
+   logic led_signal;
+   logic [3:0] s_bar;
+   
+   assign s_bar = ~s;
+  
+  // led [1:0] assignments
+   assign led[0] = s_bar[1] ^ s_bar[0];
+   assign led[1] = s_bar[3] & s_bar[2];
+   
+   // Internal high-speed oscillator
+   HSOSC #(.CLKHF_DIV(2'b01)) 
+         hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
+  
+   // Counter
+   always_ff @(posedge int_osc, negedge reset) begin
+     if(reset == 0) begin
+		 counter <= 0;
+		 led_signal <= 0;
+     end else if (counter == 5000000) begin
+		 counter <= 0;
+		 led_signal <= ~led_signal;
+	end else begin
+		counter <= counter + 1;
+	end
+   end
+  
+   // Assign led[2] output
+   assign led[2] = led_signal;
+   
+   seven_segment seven_segment_decoder(s_bar, seg);
+  
 endmodule
